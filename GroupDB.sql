47f7a08a8fb4d943fac09f8b89e71987e16142fe
chloecas <chloe.castrataro@hotmail.com>
2025-10-27 17:25:28 -0400

    Add files via upload
    latest version

--- GroupDB.sql
+++ GroupDB.sql
@@ -18,7 +18,20 @@ INSERT INTO Student VALUES('Megumi','Fushiguro','fushigurom@gmail.com',1),
 ('Kirara','Hoshi','hoshik@gmail.com',3),
 ('Momo','Nishimiya','nishimiyam@gmail.com',4),
 ('Akari','Nitta','nittaa@gmail.com',4),
-('Kiyotaka','Ijichi','ijichik@gmail.com',2);
+('Kiyotaka','Ijichi','ijichik@gmail.com',2),
+('Masamichi', 'Yaga', 'masamichiy@outlook.com', 3),
+('Yu', 'Haibara', 'yuh@outlook.com', 2),
+('Arata', 'Nitta', 'aratan@outlook.com', 1),
+('Mai', 'Zenin', 'maiz@outlook.com', 2),
+('Kokichi', 'Muta', 'kokichim@outlook.com', 2),
+('Kasumi', 'Miwa', 'kasumim@outlook.com', 2),
+('Noritoshi', 'Kamo', 'noritoshik@outlook.com', 3),
+('Takuma', 'Ino', 'takumai@outlook.com', 4),
+('Yuki', 'Tsukumo', 'yukit@outlook.com', 4),
+('Hana', 'Kurusu', 'hanak@outlook.com', 4),
+('Hiromi', 'Higuruma', 'hiromih@outlook.com', 4),
+('Rin', 'Amai', 'rina@outlook.com', 4),
+('Chizuru', 'Hari', 'chizuruh@outlook.com', 2);
 
 SELECT * FROM Student;
 
@@ -41,7 +54,7 @@ SELECT * FROM Organizer;
 CREATE TABLE Event (
     eventID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     organizerID INT NOT NULL FOREIGN KEY REFERENCES Organizer(organizerID),
-    title VARCHAR(30),
+    title VARCHAR(30) UNIQUE,
     eventDate DATE DEFAULT GETDATE(),
     location VARCHAR(25),
     capacity INT
@@ -60,8 +73,8 @@ CREATE TABLE Registration (
     registrationID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     studentID INT NOT NULL FOREIGN KEY REFERENCES Student(studentID),
     eventID INT NOT NULL FOREIGN KEY REFERENCES Event(eventID), 
-    status VARCHAR (10) UNIQUE,
-    time VARCHAR(10),
+    status VARCHAR (10),
+    time TIME(2),
 );
 
 --ENFORCING EVENT CAPACITY
@@ -88,7 +101,48 @@ END
 
 INSERT INTO Registration(studentID, eventID, status, time)
 SELECT studentID, eventID, status, time FROM inserted;
-end;
+END;
 
 INSERT INTO Registration(studentID, eventID, status, time)
-VALUES()
+VALUES(1, 2, 'Complete', CURRENT_TIMESTAMP),
+VALUES(5, 2, 'Complete', CURRENT_TIMESTAMP),
+VALUES(8, 2, 'Pending', CURRENT_TIMESTAMP),
+VALUES(9, 2, 'Complete', CURRENT_TIMESTAMP),
+VALUES(10, 2, 'Pending', CURRENT_TIMESTAMP),
+VALUES(13, 2, 'Complete', CURRENT_TIMESTAMP),
+
+VALUES(2, 1, 'Pending', CURRENT_TIMESTAMP),
+VALUES(3, 1, 'Complete', CURRENT_TIMESTAMP),
+VALUES(4, 1, 'Complete', CURRENT_TIMESTAMP),
+VALUES(6, 1, 'Pending', CURRENT_TIMESTAMP),
+VALUES(7, 1, 'Pending', CURRENT_TIMESTAMP),
+VALUES(11, 1, 'Complete', CURRENT_TIMESTAMP),
+VALUES(12, 1, 'Complete', CURRENT_TIMESTAMP),
+VALUES(13, 1, 'Complete', CURRENT_TIMESTAMP),
+VALUES(16, 1, 'Pending', CURRENT_TIMESTAMP),
+VALUES(18, 1, 'Complete', CURRENT_TIMESTAMP),
+--at capacity
+
+VALUES(14, 3, 'Complete', CURRENT_TIMESTAMP),
+VALUES(15, 3, 'Complete', CURRENT_TIMESTAMP),
+VALUES(17, 3, 'Pending', CURRENT_TIMESTAMP),
+VALUES(18, 3, 'Complete', CURRENT_TIMESTAMP),
+VALUES(19, 3, 'Complete', CURRENT_TIMESTAMP),
+VALUES(3, 3, 'Pending', CURRENT_TIMESTAMP),
+VALUES(5, 3, 'Pending', CURRENT_TIMESTAMP),
+VALUES(20, 3, 'Complete', CURRENT_TIMESTAMP),
+VALUES(7, 3, 'Complete', CURRENT_TIMESTAMP),
+VALUES(4, 3, 'Complete', CURRENT_TIMESTAMP),
+
+VALUES(19, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(21, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(22, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(23, 4, 'Pending', CURRENT_TIMESTAMP),
+VALUES(24, 4, 'Pending', CURRENT_TIMESTAMP),
+VALUES(25, 4, 'Pending', CURRENT_TIMESTAMP),
+VALUES(1, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(5, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(6, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(10, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(17, 4, 'Complete', CURRENT_TIMESTAMP),
+VALUES(20, 4, 'Complete', CURRENT_TIMESTAMP),
